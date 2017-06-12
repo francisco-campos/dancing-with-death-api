4f5618842b6fb7f3db18544a7bf88ccfb0c7001d
francisco campos <franciscojavier.c.c@gmail.com>
2017-06-12 16:42:31 -0400

    versionando api

--- app/serializers/v1/appointment_serializer.rb
+++ app/serializers/v1/appointment_serializer.rb
@@ -1,3 +1,3 @@
-class AppointmentSerializer < ActiveModel::Serializer
+class V1::AppointmentSerializer < ActiveModel::Serializer
   attributes :id, :available, :name, :email, :reservation_date, :time_start, :time_end
 end
